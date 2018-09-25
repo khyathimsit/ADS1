a8a95724f5864ae65d1a326f8aa6c426960fca3d
khyathimsit <khyathi17@msitprogram.net>
2018-09-25 18:33:27 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- CodeCamp_Percolation/Solution.java
+++ CodeCamp_Percolation/Solution.java
@@ -1,12 +1,27 @@
-import java.util.*;
-class Solution {
-    public static void main(String[] args) {
+import java.util.Scanner;
+/**.
+ * Class for solution.
+ */
+public class Solution {
+    /**.
+     * Constructs the object.
+     */
+    Solution() {
+
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = Integer.parseInt(sc.nextLine());
         Percolation obj = new Percolation(input);
-        while(sc.hasNextLine()){
+        while (sc.hasNextLine()) {
             String[] tokens = sc.nextLine().split(" ");
-            obj.open(Integer.parseInt(tokens[0])-1, Integer.parseInt(tokens[1])-1);
+            obj.open(Integer.parseInt(tokens[0]) - 1,
+                Integer.parseInt(tokens[1]) - 1);
         }
         System.out.println(obj.ispercolate());
     }
