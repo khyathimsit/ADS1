094d081dcbff45163668817bda1fe4535c39e3fe
khyathimsit <khyathi17@msitprogram.net>
2018-09-26 21:59:34 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

---
+++ Module 3/Assignments/Sorted_Array/Solution.java
@@ -0,0 +1,74 @@
+/**.
+ * { item_description }
+ */
+import java.util.Scanner;
+/**.
+ * { item_description }
+ */
+import java.util.Arrays;
+/**.
+ * Class for solution.
+ */
+class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int size1 = sc.nextInt();
+        int[] arr1 = new int[size];
+        int[] arr2 = new int[size1];
+        int[] result = new int[size + size1];
+        
+        String line = sc.next();
+        String[] tokens = line.split(",");
+        for (int n = 0; n < size; n++) {
+            arr1[n] = Integer.parseInt(tokens[n]);
+        }
+        
+        String line1 = sc.next();
+        String[] tokens1 = line1.split(",");
+        for (int n = 0; n < size1; n++) {
+            arr2[n] = Integer.parseInt(tokens1[n]);
+        }
+        
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        while (i < size && j < size1) {
+            if (arr1[i] < arr2[j]) {
+                result[k] = arr1[i];
+                k++;
+                i++;
+            } else {
+                result[k] = arr2[j];
+                k++;
+                j++;
+            }
+        }
+
+        while (i < size) {
+            result[k] = arr1[i];
+            k++;
+            i++;
+        }
+
+        while (j < size1) {
+            result[k] = arr2[j];
+            k++;
+            j++;
+        }
+
+        for (int n = 0; n < result.length - 1; n++) {
+            System.out.print(result[n] + ",");
+        }
+        System.out.println(result[result.length - 1]);
+    }
+}
