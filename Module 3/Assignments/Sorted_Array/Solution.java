89658aa5c4046ca112806057049c0c0f1db0c7c2
khyathimsit <khyathi17@msitprogram.net>
2018-09-26 22:35:49 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- Module 3/Assignments/Sorted_Array/Solution.java
+++ Module 3/Assignments/Sorted_Array/Solution.java
@@ -28,50 +28,63 @@ public final class Solution {
         int[] arr2 = new int[size1];
         int[] result = new int[size + size1];
 
-        if (size > 0) {
+        //if (size > 0) {
            String line = sc.next();
             String[] tokens = line.split(",");
             for (int n = 0; n < size; n++) {
                 arr1[n] = Integer.parseInt(tokens[n]);
             }
-        }
+        //}
 
-        if (size1 > 0) {
+        //if (size1 > 0) {
             String line1 = sc.next();
             String[] tokens1 = line1.split(",");
             for (int n = 0; n < size1; n++) {
                 arr2[n] = Integer.parseInt(tokens1[n]);
             }
-        }
+        //}
 
         int i = 0;
         int j = 0;
         int k = 0;
+        while (k < size + size1) {
+            if (i < size && j < size1) {
+                if (arr1[i] < arr2[j]) {
+                    result[k] = arr1[i];
+                    k++;
+                    i++;
+                } else {
+                    result[k] = arr2[j];
+                    k++;
+                    j++;
+                }
+            }   
+        
+            while (i < size && j < size1) {
+                if (arr1[i] < arr2[j]) {
+                    result[k] = arr1[i];
+                    k++;
+                    i++;
+                } else {
+                    result[k] = arr2[j];
+                    k++;
+                    j++;
+                }
+            }
 
-        while (i < size && j < size1) {
-            if (arr1[i] < arr2[j]) {
+            while (i < size) {
                 result[k] = arr1[i];
                 k++;
                 i++;
-            } else {
+            }
+
+            while (j < size1) {
                 result[k] = arr2[j];
                 k++;
                 j++;
             }
         }
-
-        while (i < size) {
-            result[k] = arr1[i];
-            k++;
-            i++;
-        }
-
-        while (j < size1) {
-            result[k] = arr2[j];
-            k++;
-            j++;
-        }
-
+        
         for (int n = 0; n < result.length - 1; n++) {
             System.out.print(result[n] + ",");
         }
