c6844807a406c230b5c69d9cc67906c6bf48f789
khyathimsit <khyathi17@msitprogram.net>
2018-09-26 17:16:12 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ Module 3/Assignments/3-sum/Solution.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Solution {
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] array = new int[size];
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			array[i] = sc.nextInt();
+		}
+		Arrays.sort(array);
+		for (int i = 0; i < size; i++) {
+			for (int j = i + 1; j < size; j++) {
+				int low = j + 1;
+				int high = size;
+				while (low <= high) {
+					int mid = (low + high) / 2;
+					if (-(array[i] + array[j]) == array[mid]) {
+                		count = count + 1;
+                	}
+                	else if (-(array[i] + array[j]) < array[mid]) {
+                		low = mid - 1;
+            		}
+            		else {
+                		low = mid + 1;
+            		}
+            	}
+            }
+		}
+		System.out.println(count);
+	}
+}
+	
