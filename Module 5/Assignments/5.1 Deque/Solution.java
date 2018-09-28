15e6f36e56030fed64137059e177b2112a8219bb
khyathimsit <khyathi17@msitprogram.net>
2018-09-28 17:04:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- Module 5/Assignments/5.1 Deque/Solution.java
+++ Module 5/Assignments/5.1 Deque/Solution.java
@@ -1,52 +1,68 @@
+/**.
+ * { item_description }
+ */
 import java.util.Scanner;
-class Solution {
-	Solution() {
+/**.
+ * Class for solution.
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Deque d = new Deque();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+                case"pushLeft":
+                    d.addFirst(Integer.parseInt(tokens[1]));
+                    System.out.println(d.display());
+                break;
 
-	}
-	public static void main(String[] args) {
-		Deque d = new Deque();
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
-		for (int i = 0; i < n; i++) {
-			String line = sc.nextLine();
-			String[] tokens = line.split(" ");
-			switch (tokens[0]) {
-				case"pushLeft":
-					d.addFirst(Integer.parseInt(tokens[1]));
-					System.out.println(d.display());
-				break;
+                case"pushRight":
+                    d.addLast(Integer.parseInt(tokens[1]));
+                    System.out.println(d.display());
+                break;
 
-				case"pushRight":
-					d.addLast(Integer.parseInt(tokens[1]));
-					System.out.println(d.display());
-				break;
+                case"popLeft":
+                if (!d.isEmpty()) {
+                    d.deleteFirst();
+                    System.out.println(d.display());
+                } else {
+                    System.out.println("Deck is empty");
+                }
+                break;
 
-				case"popLeft":
-				if (!d.isEmpty()) {
-					d.deleteFirst();
-					System.out.println(d.display());
-				} else {
-					System.out.println("Deck is empty");
-				}
-				break;
-
-				case"popRight":
-				    if (!d.isEmpty()) {
-				    	d.deleteLast();
-						System.out.println(d.display());
-				    } else {
-					System.out.println("Deck is empty");
-				}
-				break;
-				
-				case"size" :
-					System.out.println(d.size());
-				break;
-				
-				default:
-				break; 
-			}
-		}
-	}
+                case"popRight":
+                    if (!d.isEmpty()) {
+                        d.deleteLast();
+                        System.out.println(d.display());
+                    } else {
+                    System.out.println("Deck is empty");
+                }
+                break;
+                
+                case"size" :
+                    System.out.println(d.size());
+                break;
+                
+                default:
+                break; 
+            }
+        }
+    }
 }
