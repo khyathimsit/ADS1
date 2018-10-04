ba4b0b1cdb3c3909d37f51c7a5f8186385cd2973
khyathimsit <khyathi17@msitprogram.net>
2018-10-04 19:15:30 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- Module10/10.1 LinkedList using Recursion/Solution.java
+++ Module10/10.1 LinkedList using Recursion/Solution.java
@@ -6,52 +6,53 @@ import java.util.Scanner;
  * { item_description }
  */
 public final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
-		/**.
-		 * { item_description }
-		 */
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		LinkedListRecursion list = new LinkedListRecursion();
-		Scanner sc = new Scanner(System.in);
-		while (sc.hasNextLine()) {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        LinkedListRecursion list = new LinkedListRecursion();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
             String line = sc.nextLine();
             String[] tokens = line.split(" ");
             switch (tokens[0]) {
-            	case "insertAt":
-            		if (Integer.parseInt(tokens[1]) < 0 || Integer.parseInt(
-            			tokens[1]) > list.size() + 1) {
-            			System.out.println("Can't insert at this position.");
-            		} else {
-            			list.insertAt(Integer.parseInt(tokens[1]),
-            				Integer.parseInt(tokens[2]));
-            			System.out.println(list.toString());
-            		}
-            	break;
-            	// case"pushLeft":
+                case "insertAt":
+                    if (Integer.parseInt(tokens[1]) < 0 || Integer.parseInt(
+                        tokens[1]) > list.size() + 1) {
+                        System.out.println(
+                            "Can't insert at this position.");
+                    } else {
+                        list.insertAt(Integer.parseInt(tokens[1]),
+                            Integer.parseInt(tokens[2]));
+                        System.out.println(list.toString());
+                    }
+                break;
+                // case"pushLeft":
              //        list.insertFront(Integer.parseInt(tokens[1]));
              //        System.out.println(list.toString());
              //    break;
 
-            	case "reverse":
-            		if (!list.isEmpty()) {
-            			list.reverse();
-            			System.out.println(list.toString());
-            		} else {
-            			System.out.println("No elements to reverse.");
-            		}
-            	break;
+                case "reverse":
+                    if (!list.isEmpty()) {
+                        list.reverse();
+                        System.out.println(list.toString());
+                    } else {
+                        System.out.println("No elements to reverse.");
+                    }
+                break;
 
-            	default:
-            	break;
+                default:
+                break;
             }
         }
     }
