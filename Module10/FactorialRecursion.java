1d27e00dd840463a35a6fabe37d3553298d67a25
khyathimsit <khyathi17@msitprogram.net>
2018-10-04 22:13:35 +0530

    Activity7

---
+++ Module10/FactorialRecursion.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class FactorialRecursion {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		System.out.println(factorial(number));
+
+	}
+	public static int factorial(int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return n * factorial(n-1);
+		}
+	}
+}
