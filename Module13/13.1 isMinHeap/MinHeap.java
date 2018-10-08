709d18243d58e04cf4fd378e33d1e15ea8bf5376
khyathimsit <khyathi17@msitprogram.net>
2018-10-08 20:14:19 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Module13/13.1 isMinHeap/MinHeap.java
@@ -0,0 +1,65 @@
+/**
+ * Class for minimum heap.
+ *
+ * @param      <E>   { Generic type }.
+ */
+class MinHeap<E extends Comparable<E>> {
+    /**
+     * { Private value }.
+     */
+    private E[] array;
+    /**
+     * { Private value }.
+     */
+    private int size;
+    /**
+     * Constructs the object.
+     *
+     * @param      len   The length
+     */
+    protected MinHeap(final int len) {
+        array = (E[])new Comparable[len + 1];
+        size = 0;
+    }
+    /**
+     * { Insert method }.
+     *
+     * @param      value  The value
+     *
+     * @return     { Generic type }.
+     * 
+     * 
+     */
+    public boolean insert(final E value) {
+        array[++size] = value;
+        return (swim(size));
+    }
+    /**
+     * { For checking minheap }.
+     *
+     * @param      k1     { Integer }.
+     *
+     * @return     { Boolean }.
+     */
+    public boolean swim(final int k1) {
+        int k = k1;
+        while (k > 1 && less(k / 2, k)) {
+            k = k / 2;
+        }
+        if (k > 1) {
+            return false;
+        }
+        return true;
+    }
+    /**
+     * { For compareTo }.
+     *
+     * @param      one   One
+     * @param      two   Two
+     *
+     * @return     { boolean }.
+     */
+    public boolean less(final int one, final int two) {
+        return ((array[one].compareTo(array[two])) <= 0);
+    }
+}
---
+++ Module13/13.1 isMinHeap/Solution.java
@@ -0,0 +1,115 @@
+
+/**
+ * { Importing Scanner class }.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+class Solution {
+	/**
+	 * { Main method }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String type = scan.nextLine();
+		String[] input;
+		int testcases = Integer.parseInt(scan.nextLine());
+		int k = 0;
+		switch (type) {
+		case "String":
+			while (testcases > 0) {
+				k = 0;
+				input = scan.nextLine().split(",");
+				if (input[0].equals("")) {
+					System.out.println("false");
+				} else {
+					MinHeap<String> minheap = new MinHeap<>(input.length);
+					for (int i = 0; i < input.length; i++) {
+						if (!(minheap.insert(input[i]))) {
+							System.out.println("false");
+							k++;
+							break;
+						}
+					}
+					if (k == 0) {
+						System.out.println("true");
+					}
+				}
+				testcases--;
+			}
+			break;
+		case "Integer":
+			while (testcases > 0) {
+				k = 0;
+				input = scan.nextLine().split(",");
+				if (input[0].equals("")) {
+					System.out.println("false");
+				} else {
+					MinHeap<Integer> minheap = new MinHeap<>(input.length);
+					for (int i = 0; i < input.length; i++) {
+						if (!(minheap.insert(Integer.parseInt(input[i])))) {
+							System.out.println("false");
+							k++;
+							break;
+						}
+					}
+					if (k == 0) {
+						System.out.println("true");
+					}
+				}
+				testcases--;
+			}
+			break;
+		case "Double":
+			while (testcases > 0) {
+				k = 0;
+				input = scan.nextLine().split(",");
+				if (input[0].equals("")) {
+					System.out.println("false");
+				} else {
+					MinHeap<Double> minheap = new MinHeap<>(input.length);
+					for (int i = 0; i < input.length; i++) {
+						if (!(minheap.insert(Double.parseDouble(input[i])))) {
+							System.out.println("false");
+							k++;
+							break;
+						}
+					}
+					if (k == 0) {
+						System.out.println("true");
+					}
+				}
+				testcases--;
+
+			}
+			break;
+		case "Float":
+			while (testcases > 0) {
+				k = 0;
+				input = scan.nextLine().split(",");
+				if (input[0].equals("")) {
+					System.out.println("false");
+				} else {
+					MinHeap<Float> minheap = new MinHeap<>(input.length);
+					for (int i = 0; i < input.length; i++) {
+						if (!(minheap.insert(Float.parseFloat(input[i])))) {
+							System.out.println("false");
+							k++;
+							break;
+						}
+					}
+					if (k == 0) {
+						System.out.println("true");
+					}
+				}
+				testcases--;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
