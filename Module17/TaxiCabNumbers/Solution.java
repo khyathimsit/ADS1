0ad1b6c99036966c7e4df1aef9a0414c45fc8048
khyathimsit <khyathi17@msitprogram.net>
2018-10-12 19:59:25 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Module17/TaxiCabNumbers/Solution.java
+++ Module17/TaxiCabNumbers/Solution.java
@@ -1,102 +1,126 @@
-/**.
- * { item_description }
+/**
+ * @author : Khyathi.
+ * Date : 12th October,2018.
  */
-import java.util.*;
+import java.util.Scanner;
 /**.
  * Class for taxicab.
  */
 class Taxicab implements Comparable<Taxicab> {
-    /**.
-     * { item_description }
+    /**
+     * { variables i & j }.
      */
-    int i,j;
-    /**.
-     * { var_description }
+    private int i, j;
+    /**
+     * { variable for sum }.
      */
-    long sum;
-    /**.
+    private long sum;
+    /**
+     * { gets i }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int geti() {
+        return i;
+    }
+    /**
+     * { gets j }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int getj() {
+        return j;
+    }
+    /**
+     * { Gets the sum }.
+     *
+     * @return     The sum.
+     */
+    long getSum() {
+        return sum;
+    }
+    /**
      * Constructs the object.
      *
-     * @param      i     { parameter_description }
-     * @param      j     { parameter_description }
+     * @param      i1     { parameter_description }
+     * @param      j1     { parameter_description }
      */
-    public Taxicab(int i, int j) {
-        this.sum = (long) i*i*i + (long) j*j*j;
-        this.i = i;
-        this.j = j;
+    Taxicab(final int i1, final int j1) {
+        this.sum = (long) i1 * i1 * i1 + (long) j1 * j1 * j1;
+        this.i = i1;
+        this.j = j1;
     }
-    /**.
-     * { function_description }
+    /**
+     * { compares the sum value and returns the greatest }.
      *
      * @param      that  The that
      *
      * @return     { description_of_the_return_value }
      */
-    public int compareTo(Taxicab that) {
+    public int compareTo(final Taxicab that) {
         if (this.sum < that.sum) {
             return -1;
         } else if (this.sum > that.sum) {
             return +1;
-        }
-        else {
+        } else {
             return  0;
         }
     }
-    /**.
-     * Returns a string representation of the object.
+    /**
+     * { Returns a string representation of the object }.
      *
      * @return     String representation of the object.
      */
     public String toString() {
         return i + "^3 + " + j + "^3";
     }
-
 }
 /**.
- * Class for solution.
+ * { item_description }
  */
-class Solution {
-    /**.
-     * { function_description }
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * Main Function.
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLine()){
+        final int n = 1000;
+        while (sc.hasNextLine()) {
             String[] input = sc.nextLine().split(" ");
-            int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
-            int n = 1000;
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
             MinPQ<Taxicab> pq = new MinPQ<Taxicab>();
             for (int i = 1; i <= n; i++) {
                 pq.insert(new Taxicab(i, i));
             }
-        int pair = 1;
-        Taxicab prev = new Taxicab(0, 0);
-        int pairCount = 0;
-        while (!pq.isEmpty()) {
-            Taxicab curr = pq.delMin();
-            if (prev.sum == curr.sum) {
-                pair++;
-                if (pair == M){
-                    pairCount = pairCount+1;
+            int pair = 1;
+            Taxicab prev = new Taxicab(0, 0);
+            int pairCount = 0;
+            while (!pq.isEmpty()) {
+                Taxicab curr = pq.delMin();
+                if (prev.getSum() == curr.getSum()) {
+                    pair++;
+                    if (pair == b) {
+                        pairCount = pairCount + 1;
+                    }
+                    if (pairCount == a) {
+                        System.out.println(prev.getSum());
+                        break;
+                    }
+                } else {
+                    pair = 1;
                 }
-                if(pairCount == N){
-                    System.out.println(prev.sum);
-                    break;
+                prev = curr;
+                if (curr.getj() < n) {
+                    pq.insert(new Taxicab(curr.geti(), curr.getj() + 1));
                 }
-            }
-            else {
-                pair = 1;
-            }
-            prev = curr;
-            if (curr.j < n){
-                pq.insert(new Taxicab(curr.i, curr.j + 1));
             }
         }
-
     }
-
-}
 }
