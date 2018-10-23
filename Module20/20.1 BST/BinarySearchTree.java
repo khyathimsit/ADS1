e6a6d41e224dffd0e295d4a38bb54066e18c2763
khyathimsit <khyathi17@msitprogram.net>
2018-10-23 21:47:11 +0530

    ": 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Module20/20.1 BST/BinarySearchTree.java
+++ Module20/20.1 BST/BinarySearchTree.java
@@ -365,7 +365,7 @@ class BinarySearchTree<Key extends Comparable<Key>, Value> {
     }
 
     public void delete(BookList key) {
-        delete(root, key);
+        root = delete(root, key);
     }
 
     public Node delete(Node x, BookList key) {
