a7c3777f8992fab9481d55a3df5e521496bd1ee4
khyathimsit <khyathi17@msitprogram.net>
2018-10-25 22:22:34 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module22/22.1 Ransom Note/Solution.java
+++ Module22/22.1 Ransom Note/Solution.java
@@ -62,3 +62,109 @@ public final class Solution {
         }
     }
 }
+/**.
+ * Class for hash table.
+ */
+class HashTable {
+    /**.
+     * { item_description }
+     */
+    private final int minusone = -1;
+    /**.
+     * { var_description }
+     */
+    private final int FIFTY_1 = 50;
+    /**.
+     * { var_description }
+     */
+    private final int HEXA_1 = 0x7fffffff;
+    /**.
+     * { var_description }
+     */
+    public int NUM_1 = FIFTY_1;
+    /**.
+     * { var_description }
+     */
+    private Node[] s = new Node[NUM_1];
+    /**.
+     * Class for node.
+     */
+    public static class Node {
+        /**.
+         * { var_description }
+         */
+        private String key;
+        /**.
+         * { var_description }
+         */
+        private int val;
+        /**.
+         * { var_description }
+         */
+        private Node next;
+        /**.
+         * Constructs the object.
+         *
+         * @param      k   The key
+         * @param      v   The value
+         * @param      n     { parameter_description }
+         */
+        protected Node(final String k, final int v, final Node n) {
+            this.key = k;
+            this.val = v;
+            this.next = n;
+        }
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private int hash(final String key) {
+        return (key.hashCode() & HEXA_1) % NUM_1;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
+    public void put(final String key, final int val) {
+        int i = hash(key);
+        for (Node x = s[i]; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        s[i] = new Node(key, val, s[i]);
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int get(final String key) {
+        int i = hash(key);
+        for (Node x = s[i]; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                return x.val;
+            }
+        }
+        return minusone;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      k     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean contains(final String k) {
+        return get(k) != minusone;
+    }
+}
