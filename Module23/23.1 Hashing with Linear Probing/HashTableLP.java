04834bf815090fc061d380a7b8c703eb93edc7cc
khyathimsit <khyathi17@msitprogram.net>
2018-10-26 17:52:33 +0530

    "ads1_m23_a1: 3/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module23/23.1 Hashing with Linear Probing/HashTableLP.java
+++ Module23/23.1 Hashing with Linear Probing/HashTableLP.java
@@ -60,7 +60,7 @@ public class HashTableLP<Key, Value> {
 
     // hash function for keys - returns value between 0 and M-1
     private int hash(Key key) {
-        return (key.hashCode() & 0x7fffffff) % m;
+        return (key.hashCode() * 11) % m;
     }
 
     // resizes the hash table to the given capacity by re-hashing all of the keys
