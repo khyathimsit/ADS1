467514cf56ea2cdc313a7e89c952ef5cf04e241c
khyathimsit <khyathi17@msitprogram.net>
2018-10-27 00:13:52 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Module23/23.1 Hashing with Linear Probing/Solution.java
+++ Module23/23.1 Hashing with Linear Probing/Solution.java
@@ -1,46 +1,38 @@
-/**.
- * { item_description }
- */
 import java.util.Scanner;
-/**.
- * { item_description }
+/**
+ * Class for solution.
  */
-public final class Solution {
-    /**.
+final class Solution {
+    /**
      * Constructs the object.
      */
-    private Solution() {
-        /**.
-         * { item_description }
-         */
-    }
-    /**.
-     * { function_description }
+    private Solution() { }
+    /**
+     * { Main Function }.
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        HashTableLP lp = new HashTableLP();
-        int n = sc.nextInt();
-        while(sc.hasNext()) {
-            String input = sc.nextLine();
-            String[] tokens = input.split(" ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        LinearProbingHashST<String, Integer> hashTable =
+            new LinearProbingHashST<>();
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
             switch (tokens[0]) {
-                case "put":
-                // System.out.println("input entered");
-                    lp.put(tokens[1], Integer.parseInt(tokens[2]));
+            case "put":
+                hashTable.put(tokens[1], Integer.parseInt(tokens[2]));
                 break;
-                case "get":
-                    System.out.println(lp.get(tokens[1]));
+            case "get":
+                System.out.println(hashTable.get(tokens[1]));
                 break;
-                case "delete":
-                    lp.delete(tokens[1]);
+            case "delete":
+                hashTable.delete(tokens[1]);
                 break;
-                case "display":
-                    lp.display();
+            case "display":
+                hashTable.display();
                 break;
-                default:
+            default:
                 break;
             }
         }
